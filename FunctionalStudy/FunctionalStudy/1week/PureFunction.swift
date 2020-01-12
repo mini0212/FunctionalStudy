f99c587db5c22ea34fae6a793852cd18b8d5ab3d
Min <min_i0212@hanmail.net>
2020-01-12 13:54:43 +0900

    Add Pure Function

---
+++ FunctionalStudy/FunctionalStudy/1week/PureFunction.swift
@@ -0,0 +1,13 @@
+//
+//  PureFunction.swift
+//  FunctionalStudy
+//
+//  Created by Min on 2020/01/12.
+//  Copyright © 2020 Mins. All rights reserved.
+//
+
+import Foundation
+
+class PureFunction {
+    
+}
